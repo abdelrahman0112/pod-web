b8e3a261e3e1bcebb4e44ff472f43287596861dd
abdelrahman hamdy <abdelrahmanhamdy320@gmail.com>
2025-11-02 22:44:28 +0200

    fix(deploy): remove seeder from startup script

--- docker/start.sh
+++ docker/start.sh
@@ -6,8 +6,8 @@ set -e
 # Wait for the database to be ready
 sleep 5
 
-# Run database migrations and seed
-php artisan migrate:fresh --seed --force
+# Run database migrations ONLY. Seeding can be done manually later.
+php artisan migrate:fresh --force
 
 # Cache configuration
 php artisan config:cache
