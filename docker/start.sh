fa00e7c2a6f820ffa499073f73c41de692253d22
abdelrahman hamdy <abdelrahmanhamdy320@gmail.com>
2025-11-02 22:48:05 +0200

    refactor(deploy): use release_command for migrations

--- docker/start.sh
+++ docker/start.sh
@@ -3,19 +3,9 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Wait for the database to be ready
-sleep 5
-
-# Run database migrations ONLY. Seeding can be done manually later.
-php artisan migrate:fresh --force
-
 # Cache configuration
 php artisan config:cache
-
-# Cache routes
 php artisan route:cache
-
-# Cache views
 php artisan view:cache
 
 # Start Supervisor
